38ffee95ddaecb58c305718ac05044a4af28a686
Nate <nathan.harris@force5solutions.com>
2020-12-20 16:13:05 -0700

    Updated info, prep for PyPi release

--- tautulli/_info.py
+++ tautulli/_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 
 __title__ = "tautulli"
 __author__ = 'Nate Harris'
@@ -7,4 +7,4 @@ __github_username__ = "nwithan8"
 __copyright__ = "Copyright © 2020 - Nate Harris"
 __license__ = 'GNU General Public License v3 (GPLv3)'
 __description__ = "A complete Python client for Tautulli's API"
-__keywords__ = ["keywords", "related", "to", "this", "package"]
+__keywords__ = ["Tautulli", "API", "client", "Plex", "PMS", "Plex Media Server", "media", "server", "JSON"]
