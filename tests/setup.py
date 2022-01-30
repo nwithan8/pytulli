d2eb7ce85d6203949b4cc31b81330118ce515b5e
Nate Harris <nharris@easypost.com>
2022-01-30 00:43:26 -0700

    - Prepare GitHub Action tests

--- tests/setup.py
+++ tests/setup.py
@@ -5,15 +5,18 @@ from dotenv import load_dotenv
 import tautulli
 
 
-def raw_client(bypass_key: bool = False) -> tautulli.RawAPI:
+def raw_client(no_key: bool = False) -> tautulli.RawAPI:
     load_dotenv()
     url = os.getenv("T_URL")
     if not url:
         raise ValueError("T_URL is not set")
-    key = os.getenv("T_KEY")
-    if not key:
-        if bypass_key:
-            key = ""
-        else:
-            raise ValueError("T_KEY is not set")
-    return tautulli.RawAPI(base_url=url, api_key=key, verbose=True)
+    if no_key:
+        return tautulli.RawAPI(base_url=url, api_key="placeholder")
+    else:
+        key = os.getenv("T_KEY")
+        if not key:
+            temp_client = tautulli.RawAPI(base_url=url, api_key="placeholder")
+            key = temp_client.get_api_key()
+        if not key:
+            raise ValueError("T_KEY is not set and could not be retrieved from server")
+        return tautulli.RawAPI(base_url=url, api_key=key, verbose=True)
--- tests/test_jsonapi.py
+++ tests/test_jsonapi.py
@@ -2,7 +2,15 @@ from tests.setup import raw_client
 
 
 def test_get_api_key():
-    no_auth_client = raw_client(bypass_key=True)
-    key = no_auth_client.get_api_key()
+    no_key_client = raw_client(no_key=True)
+    key = no_key_client.get_api_key()
     assert key is not None
     assert key != ''
+
+
+def test_ping():
+    client = raw_client()
+    res = client.ping()
+    assert res is not None
+    assert type(res) == bool
+    assert res is True
