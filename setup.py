665c5e37d089c3cf186443749d566905f25e8419
Nate Harris <nharris@easypost.com>
2022-04-09 17:51:21 -0600

    - Fix dependency locks

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 import setuptools
+
 from tautulli._info import __title__, __description__, __version__, __author__, __author_email__, __license__, \
-    __keywords__, __github_username__, __github_repo__, __copyright__
+    __keywords__, __github_username__, __github_repo__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", 'r') as fh:
-    requirements = fh.read().splitlines()
+REQUIREMENTS = [
+    "objectrest~=1.2",
+    "pydantic~=1.9",
+    "pytz~=2022.1",
+    "python-dotenv~=0.20",
+    "packaging~=21.3",
+    "typing-extensions"
+]
+
+DEV_REQUIREMENTS = [
+    "black",
+    "flake8",
+    "isort",
+    "pytest-cov==3.*",
+    "pytest-vcr==1.*",
+    "pytest==7.*",
+    "types-requests",
+    "types-urllib3",
+    "vcrpy==4.*",
+]
 
 setuptools.setup(
     name=__title__,
@@ -21,7 +40,11 @@ setuptools.setup(
     url=f'https://github.com/{__github_username__}/{__github_repo__}',
     download_url=f'https://github.com/{__github_username__}/{__github_repo__}/archive/{__version__}.tar.gz',
     keywords=__keywords__,
-    install_requires=requirements,
+    install_requires=REQUIREMENTS,
+    extras_require={
+        "dev": DEV_REQUIREMENTS,
+    },
+    test_suite="test",
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Development Status :: 4 - Beta',
@@ -37,5 +60,5 @@ setuptools.setup(
         'Topic :: Internet :: WWW/HTTP',
         'Operating System :: OS Independent'
     ],
-    python_requires='>=3.7'
+    python_requires='>=3.7, <4'
 )
