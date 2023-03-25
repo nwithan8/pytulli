8b60d541ec7f0b67034edae42983b528f29a7218
nwithan8 <nwithan8@users.noreply.github.com>
2023-03-25 13:17:07 -0600

    - I hate this

--- tautulli/__init__.py
+++ tautulli/__init__.py
@@ -1,14 +1,2 @@
-import json
-import os
-from typing import List
-
 from tautulli.api import ObjectAPI
 from tautulli.api import RawAPI
-
-def __supported_api_versions__() -> List[str]:
-    with open(os.path.join(os.path.dirname(__file__), "API_VERSIONS.json")) as f:
-        versions = f.read()
-        return json.loads(versions)
-
-def __min_api_version__() -> str:
-    return __supported_api_versions__()[0]
--- tautulli/_info.py
+++ tautulli/_info.py
@@ -1,11 +1,12 @@
+import json
 import os
 from typing import List
 
 
 def __supported_api_versions__() -> List[str]:
-    with open(os.path.join(os.path.dirname(__file__), "API_VERSIONS")) as f:
-        return f.read().splitlines()
-
+    with open(os.path.join(os.path.dirname(__file__), "API_VERSIONS.json")) as f:
+        versions = f.read()
+        return json.loads(versions)
 
 def __min_api_version__() -> str:
     return __supported_api_versions__()[0]
